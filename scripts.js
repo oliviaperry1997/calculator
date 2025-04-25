c10433e43a004ba0ff4166cd3725e959172541fd
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-04-25 14:46:19 +0100

    Add function operate and declare variables
    This commit adds the function operate, which takes three arguments to
    operate on two numbers and call one of the four functions created in
    the previous commit depending on which operator is passed into the
    middle argument. It also declares three variables which can be passed
    into the function later on.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a,b) {
     return a+b;
 };
@@ -12,4 +16,15 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a/b;
+}
+
+function operate(x,op,y) {
+    if (op === '+') {
+        return add(x,y)};
+    if (op === '-') {
+        return subtract(x,y)};
+    if (op === '*') {
+        return multiply(x,y)};
+    if (op === '/') {
+        return divide(x,y)};
 }
